26a75b93fd4af5d8fc95dd8bd363363e138609a6
Matheus Assumpção <matheusassumpcap@gmail.com>
2024-12-11 14:35:45 -0300

    api

---
+++ src/config/dbConnect.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+async function conectaNaDataBase() {
+    mongoose.connect("mongodb+srv://admin:bruce2024@cluster0.7x5ru.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    return mongoose.connection;
+};
+
+export default conectaNaDataBase;
+
+
