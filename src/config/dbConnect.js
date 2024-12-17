4c55c0c5816327cd0101acfe423c5d9f2e95d69f
Matheus Assumpção <matheusassumpcap@gmail.com>
2024-12-17 15:11:42 -0300

    finalizando API livros

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 async function conectaNaDataBase() {
-    mongoose.connect("mongodb+srv://admin:bruce2024@cluster0.7x5ru.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    mongoose.connect("mongodb+srv://admin:bruce2024@cluster0.7x5ru.mongodb.net/livraria?retryWrites=true&w=majority&appName=Cluster0");
     return mongoose.connection;
 };
 
